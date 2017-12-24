1a1163cef978f95ea86e240db6a68e92014d19b9
exit <yeshsurya@gmail.com>
2017-12-24 11:01:07 +0530

    Adding number chapter

---
+++ Numbers/parseFromString.scala
@@ -0,0 +1,14 @@
+"100".toInt
+"100".toDouble
+"100".toFloat
+"10".toLong
+"10".toShort
+"1".toByte
+
+///Handling base and radix
+
+Integer.parseInt("1",2)
+Integer.parseInt("10",2)
+Integer.parseInt("100",2)
+Integer.parseInt("1",8)
+Integer.parseInt("10",8)
